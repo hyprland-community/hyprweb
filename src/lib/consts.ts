a54697d86fade09c0a7d85d5973c9c0d7c0ebdb7
Yavor Kolev <yavko@outlook.com>
2024-04-27 18:47:05 -0700

    Fix discord

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,6 +1,6 @@
 export const hyprThemeColors = ["#0ee7f6", "#512eff", "#FF1493"] as const
 
-export const discordUrl = "https://discord.gg/HgT7ZgPG"
+export const discordUrl = "https://discord.gg/zzWqvcKRMy"
 export const githubUrl = "https://github.com/hyprland-community/"
 export const hyprthemeUrl = "/hyprtheme"
 export const projectsUrl = "/projects"
